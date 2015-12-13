4324c7944009708a289cff27c93c0c877dca0a14
jeff tindell (home) <jeff@netaura.com>
2015-12-12 21:40:58 -0500

    added 'droll' library
    Why:
    
    * its pretty sick...
    
    This change addresses the need by:
    
    *

--- api/controllers/RollController.js
+++ api/controllers/RollController.js
@@ -8,7 +8,79 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+var crit_miss_texts = ['WOMP womp... Critical Miss!', 'Yay! Rolled a 1... oh wait, that\'s bad...',
+                  'wow... you are terrible. Critical Miss.', 'Merry CritMiss!']
+
+var crit_hit_urls = ['https://i.ytimg.com/vi/fGl4LOAgW50/maxresdefault.jpg']
+
+var crit_hit_texts = ['OUCH, Critical Hit!', 'Take that baddie, Critical Hit.',
+                      'You literally couldn\'t have rolled better... Critical Hit!', 'DAAAA']
+
+
+var droll = require('droll');
+
 module.exports = {
+
+  dice2: function(req, res){
+    // expected input: 3d6+5 w
+    nums = req.param('text').split(' ')
+    //nums[0] == 3d6+5, [1] == 'w'
+
+    whisper = false
+    operator = '+'
+    global_add = '0'
+
+    // if w was passed in, whisper.
+    if (nums[1]){
+      if (nums[1].toUpperCase() == 'W' ) {
+        whisper = true
+      }
+    }
+    sides = nums[0].toUpperCase().split('D')[1].split(/[+-]/)[0]
+    var result = droll.roll(nums[0])
+
+    if(result){
+      rolls = result['rolls']
+      mod = result['modifier']
+      total = result['total']
+      attachments_text = ''
+      if(rolls.length == 1){
+        attachments_text = 'You rolled: ' + rolls[0] + '. '
+        if(mod > 0  ){
+          attachments_text += 'I added '+ mod +' to the final result.'
+        } else if (mod < 0) {
+          attachments_text += 'I subtracted '+ Math.abs(mod) +' from the final result.'
+        }
+        if (sides == 20){
+          if(rolls[0] == 20){
+            attachments_text =  crit_hit_texts[Math.floor(Math.random()*crit_hit_texts.length)] + '\n' +
+                                crit_hit_urls[Math.floor(Math.random()*crit_hit_urls.length)];
+            total = 20
+          } else if (rolls[0] == 1){
+            attachments_text =  crit_miss_texts[Math.floor(Math.random()*crit_miss_texts.length)];
+            total = 1
+          }
+        }
+      } else {
+        attachments_text = 'Your rolls: ' + rolls + '. I added ' + mod + ' to the final result.'
+      }
+
+    } else {
+      return res.send("You typed something wrong... make sure you type in the format xdy+z (*ex: 3d6+2*)")
+    }
+    if (whisper){
+      response_type = 'ephemeral'
+    } else {
+      response_type = 'in_channel'
+    }
+
+    return res.send({'response_type': response_type,
+                      'text' : "*"+ total +"*",
+                      'attachments' : [ {"text" : attachments_text}]
+                      })
+
+  },
+
 
   dice: function (req, res){
     if (req.param('text')){
